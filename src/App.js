08b3d476b8d83b744c849835b7a3a76be1fa0e7f
Everson <sousekjuniorr@outlook.com>
2023-11-15 21:22:54 -0300

    Criado esquelo da página principal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
