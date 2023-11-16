334fe85144ff74c9f7635e4d7cff50c8b1817d19
Everson <sousekjuniorr@outlook.com>
2023-11-15 21:45:22 -0300

    Dar get de informações do banco na página principal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+    const [usuarios,setUsuarios] = useState([])
+    
+    useEffect(() => {
+            carregaUsuarios();
+    },[])
+
+    const carregaUsuarios =async () => {
+        const resultado =await axios.get("http://localhost:8080/usuarios")
+        setUsuarios(resultado.data)
+    }
+
   return (
     <div className='container'>
         <div className='py-4'>
@@ -8,29 +21,23 @@ export default function Home() {
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Nome</th>
+      <th scope="col">NomeDeUsuario</th>
+      <th scope="col">email</th>
+      <th scope="col">Ação</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+   {
+        usuarios.map((usuario, index) =>(
+            <tr key={index}>
+                <th scope='row'>{index + 1 }</th>
+                <td>{usuario.nome}</td>
+                <td>{usuario.nomeDeUsuario}</td>
+                <td>{usuario.email}</td>
+            </tr>
+        ))
+   }
   </tbody>
 </table>
         </div>
